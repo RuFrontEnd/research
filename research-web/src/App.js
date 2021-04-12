0f02f9291f0ed8df61a1c403e229ad2318b7a097
RuFrontEnd <50475511+RuFrontEnd@users.noreply.github.com>
2021-04-12 15:51:40 +0800

    center the container / create input

--- research-web/src/App.js
+++ research-web/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// tutorial https://pjchender.blogspot.com/2019/01/js-javascript-input-file-upload-file.html
+import { useEffect, useState, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const $fileUploader = useRef();
+  const [addFileTime, setAddFileTime] = useState(0);
+
+  useEffect(() => {
+    console.log("$fileUploader", $fileUploader);
+    console.log("$fileUploader.current.dataset", $fileUploader.current.dataset);
+  }, [addFileTime]);
+
+  useEffect(() => {
+    console.log("$fileUploader.current.files", $fileUploader.current.files);
+  }, [addFileTime]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* multiple => 屬性可以一次上傳多個檔案 */}
+      {/* accept => 屬性可以限制上傳檔案的類型 */}
+      <div className="fileContainer">
+        <div className="fileWarp">
+          <input
+            type="file"
+            id="file-uploader"
+            data-target="file-uploader"
+            accept="image/*"
+            multiple="multiple"
+            ref={$fileUploader}
+            onChange={() => {
+              setAddFileTime(addFileTime + 1);
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }

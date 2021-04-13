9339b5ab691aa3fb580f436ec2073ea34e3be853
RuFrontEnd <50475511+RuFrontEnd@users.noreply.github.com>
2021-04-13 13:07:43 +0800

    new cropper.js

--- research-web/src/App.js
+++ research-web/src/App.js
@@ -1,4 +1,6 @@
 // tutorial https://pjchender.blogspot.com/2019/01/js-javascript-input-file-upload-file.html
+// Cropper.js https://github.com/fengyuanchen/cropperjs
+// Cropper.js tutorial https://www.youtube.com/watch?v=hM9uKmy-BQQ&ab_channel=ThePolyglotDeveloper
 import { useState, useRef } from "react";
 import "./App.css";
 

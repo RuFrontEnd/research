fbfef3e7de9372adf14db1340125b8b4b7bb1cc2
RuFrontEnd <k718198@gmail.com>
2021-04-13 16:55:16 +0800

    revise App.css

--- research-web/src/uploadFile/UploadFile.js
+++ research-web/src/uploadFile/UploadFile.js
@@ -1,4 +1,4 @@
-// tutorial https://pjchender.blogspot.com/2019/01/js-javascript-input-file-upload-file.html
+// upload file tutorial https://pjchender.blogspot.com/2019/01/js-javascript-input-file-upload-file.html
 // Cropper.js https://github.com/fengyuanchen/cropperjs
 // Cropper.js tutorial https://www.youtube.com/watch?v=hM9uKmy-BQQ&ab_channel=ThePolyglotDeveloper
 import { useEffect, useState, useRef } from "react";

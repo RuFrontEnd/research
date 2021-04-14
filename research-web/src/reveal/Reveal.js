d67b7a095d7516de2e026002c1b5476cd50faaf9
RuFrontEnd <k718198@gmail.com>
2021-04-14 16:03:56 +0800

    use reveal effect

--- research-web/src/reveal/Reveal.js
+++ research-web/src/reveal/Reveal.js
@@ -1,13 +1,42 @@
 // npm i react-reveal --save (如有版本問題使用 npm i react-reveal --save --force)
+// yarn add react-reveal
 // https://www.react-reveal.com/
 
-import { useEffect, useState, useRef } from "react";
 import "./Reveal.css";
+import logo from "../assets/logo.png";
+import Zoom from "react-reveal/Zoom";
+import Fade from "react-reveal/Fade";
+import Slide from "react-reveal/Slide"
 
 function Reveal() {
   return (
     <>
-      <div>123</div>
+      <div>
+        <header className="App-header">
+          <h1 className="App-title">01</h1>
+          <Zoom>
+            <img src={logo} className="App-logo" alt="logo" />
+          </Zoom>
+          <h1 className="App-title">02</h1>
+          <Fade left>
+            <img src={logo} className="App-logo" alt="logo" />
+          </Fade>
+          <h1 className="App-title">03</h1>
+          <Fade top big cascade>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <img src={logo} className="App-logo" alt="logo" />
+              <img src={logo} className="App-logo" alt="logo" />
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+          </Fade>
+          <h1 className="App-title">04</h1>
+          <Slide bottom>
+            <div>
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+          </Slide>
+        </header>
+      </div>
     </>
   );
 }

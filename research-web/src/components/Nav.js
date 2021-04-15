39987125dc5b18a00932fd282268f7cfb11e7185
RuFrontEnd <k718198@gmail.com>
2021-04-15 15:40:51 +0800

    change class component into function component

--- research-web/src/components/Nav.js
+++ research-web/src/components/Nav.js
@@ -1,11 +1,44 @@
-import { DatePicker } from "antd"; // npm install antd 或 yarn add antd
-import "antd/dist/antd.css";
-import { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
+
+import "components/Nav.css";
+import { Row, Col, Slider } from "antd";
+
+const gutters = {};
+const vgutters = {};
+const colCounts = {};
+
+[8, 16, 24, 32, 40, 48].forEach((value, i) => {
+  gutters[i] = value;
+});
+[8, 16, 24, 32, 40, 48].forEach((value, i) => {
+  vgutters[i] = value;
+});
+[2, 3, 4, 6, 8, 12].forEach((value, i) => {
+  colCounts[i] = value;
+});
 
 function Nav() {
+  const [gutterKey, setGutterKey] = useState(1);
+  const [vgutterKey, setVgutterKey] = useState(1);
+  const [colCountKey, setColCountKey] = useState(2);
+
+  const cols = [];
+  const colCount = colCounts[colCountKey];
+  let colCode = "";
+  for (let i = 0; i < colCount; i++) {
+    cols.push(
+      <Col key={i.toString()} span={24 / colCount}>
+        <div>Column</div>
+      </Col>
+    );
+    colCode += `<Col span={${24 / colCount}} />\n`;
+  }
   return (
     <div>
-      <DatePicker></DatePicker>
+      <Row gutter={[gutters[gutterKey], vgutters[vgutterKey]]}>
+        {cols}
+        {cols}
+      </Row>
     </div>
   );
 }

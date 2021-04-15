62e7cec86f01f750471bae42f375619005b385dc
RuFrontEnd <k718198@gmail.com>
2021-04-15 14:46:57 +0800

    create react router

--- research-web/src/App.js
+++ research-web/src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom"; // npm install react-router-dom
 import Reveal from "./reveal/Reveal";
 
 function App() {
   return (
-    <>
-      <Reveal />
-    </>
+    <BrowserRouter>
+     <Switch>
+        <Route exact path="/Reveal"></Route>
+      </Switch>
+      <Switch>
+        <Route exact path="/Reveal"></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 

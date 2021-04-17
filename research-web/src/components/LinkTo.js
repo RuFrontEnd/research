87b10de445e64da7bab7ea6d1128e08e4de2bd49
RuFrontEnd <k718198@gmail.com>
2021-04-17 17:18:16 +0800

    styling card

--- research-web/src/components/LinkTo.js
+++ research-web/src/components/LinkTo.js
@@ -1,27 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "components/LinkTo.css";
 
 function LinkTo(props, children) {
   const { address, name } = props;
   return (
     <Link to={address}>
-      <div
-        style={{
-          width: "100%",
-          height: "100%",
-          boxShadow: "5px 2px 5px #00000055",
-          display: "flex",
-          justifyContent: "center",
-          borderLeft: "10px solid #FF4D00",
-          borderRadius: "5px",
-          fontSize: "20px",
-          fontFamily: "sans-serif",
-          color: "#FF4D00",
-          letterSpacing: "0.5px",
-        }}
-      >
-        {name}
-      </div>
+      <div id={"LinkToContainer"}>{name}</div>
     </Link>
   );
 }

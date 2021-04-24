b95114b516fc6bc1b8c4405028fdfa8aa05ceaac
RuFrontEnd <k718198@gmail.com>
2021-04-24 14:23:30 +0800

    import fontAwesome

--- research-web/src/components/Carousel/CarouselModule.js
+++ research-web/src/components/Carousel/CarouselModule.js
@@ -1,6 +1,9 @@
 import { useEffect, useState, useRef } from "react";
 import CarouselItem from "components/carousel/CarouselItem";
 import "components/carousel/Carousel.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 function Carousel(props) {
   const { activation, cards } = props;
@@ -26,19 +29,21 @@ function Carousel(props) {
   };
 
   useEffect(() => {
-    // console.log(generateItems())
+    console.log();
   }, []);
 
   return (
     <div id="carousel" className="noselect">
       <div className="arrow arrow-left">
-        <i className="fi-arrow-left"></i>
+        <FontAwesomeIcon icon={faArrowLeft} />
       </div>
       {/* <ReactCSSTransitionGroup transitionName={this.state.direction}> */}
       {generateItems()}
       {/* </ReactCSSTransitionGroup> */}
       <div className="arrow arrow-right">
-        <i className="fi-arrow-right"></i>
+        <i className="fi-arrow-right">
+          <FontAwesomeIcon icon={faArrowRight} />
+        </i>
       </div>
     </div>
   );

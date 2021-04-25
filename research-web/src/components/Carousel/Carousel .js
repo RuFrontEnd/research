4cfc47e93b96aabb7735ad666e06456905c6a7a2
RuFrontEnd <k718198@gmail.com>
2021-04-25 14:16:05 +0800

    add move logic

--- research-web/src/components/Carousel/Carousel .js
+++ research-web/src/components/Carousel/Carousel .js
@@ -1,8 +1,6 @@
-import { useEffect, useState, useRef } from "react";
 import CarouselModule from "components/carousel/CarouselModule";
 
-function Carousel(props) {
-  console.log(props);
+function Carousel() {
   const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return <CarouselModule activation={0} cards={cards} />;
 }
--- research-web/src/components/Carousel/CarouselModule.js
+++ research-web/src/components/Carousel/CarouselModule.js
@@ -1,23 +1,22 @@
+import React from "react";
 import { useEffect, useState, useRef } from "react";
+import { CSSTransitionGroup } from "react-transition-group"; // npm install react-transition-group@1.x --save / yarn add react-transition-group@1.x
 import CarouselItem from "components/carousel/CarouselItem";
 import "components/carousel/Carousel.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-// let ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
-
 function Carousel(props) {
-  console.log("props", props);
   const { activation, cards } = props;
   const [active, setActive] = useState(activation);
   const [items, setItems] = useState(cards);
+  const [direction, setDirection] = useState("");
 
   const generateItems = () => {
-    let items = [];
+    let components = [];
     let level;
     let index;
-    let tmp = active - 2;
     for (let i = active - 2; i < active + 3; i++) {
       index = i;
       if (i < 0) {
@@ -25,33 +24,48 @@ function Carousel(props) {
       } else if (i >= items.length) {
         index = i % items.length;
       }
-      level = active - tmp;
-      items.push(<CarouselItem key={index} id={items[index]} level={level} />);
+      level = active - i;
+      components.push(
+        <CarouselItem key={index} id={items[index]} level={level} />
+      );
     }
-    return items;
+    return components;
   };
 
-  useEffect(() => {
-    // console.log(generateItems());
-  }, []);
+  const moveLeft = () => {
+    let newActive = active;
+    newActive--;
+    setActive(newActive < 0 ? items.length - 1 : newActive);
+    setDirection("left");
+  };
 
+  const moveRight = () => {
+    let newActive = active;
+    newActive++;
+    setActive(newActive < 0 ? items.length - 1 : newActive);
+    setDirection("right");
+  };
+  
   return (
     <div id="carousel" className="noselect">
-      <div className="arrow arrow-left">
+      <div
+        className="arrow arrow-left"
+        onClick={() => {
+          moveLeft();
+        }}
+      >
         <FontAwesomeIcon icon={faArrowLeft} />
       </div>
-      {/* <ReactCSSTransitionGroup transitionName={this.state.direction}> */}
-      {/* {generateItems()} */}
-      <CarouselItem key={1} id={1} level={0} />
-      <CarouselItem key={2} id={2} level={1} />
-      <CarouselItem key={3} id={3} level={2} />
-      <CarouselItem key={4} id={4} level={-1} />
-      <CarouselItem key={4} id={5} level={-2} />
-      {/* </ReactCSSTransitionGroup> */}
-      <div className="arrow arrow-right">
-        <i className="fi-arrow-right">
-          <FontAwesomeIcon icon={faArrowRight} />
-        </i>
+      <CSSTransitionGroup transitionName={direction}>
+        {generateItems()}
+      </CSSTransitionGroup>
+      <div
+        className="arrow arrow-right"
+        onClick={() => {
+          moveRight();
+        }}
+      >
+        <FontAwesomeIcon icon={faArrowRight} />
       </div>
     </div>
   );

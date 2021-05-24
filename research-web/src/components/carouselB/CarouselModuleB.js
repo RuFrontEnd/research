28a4a40039d182d2306ad4000537b103b10590a2
RuFrontEnd <k718198@gmail.com>
2021-05-24 18:20:43 +0800

    create carouselmodule

---
+++ research-web/src/components/carouselB/CarouselModuleB.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect, useRef } from "react";
+import "components/carouselB/CarouselB.scss";
+import Radium from "radium"; // 可以使inline-style有media-qurey功能
+import { ReactComponent as ArrowLeft } from "assets/arrowLeft.svg";
+import { ReactComponent as ArrowRight } from "assets/arrowRight.svg";
+
+function CarouselModuleB(props) {
+  const [direction, setDirection] = useState(-1);
+  const $slider = useRef();
+  const $carousel = useRef();
+  // const [items, setItems] = useState(["3", "1", "2"]);
+  const [items, setItems] = useState(["4", "5", "1", "2", "3"]);
+  // const [items, setItems] = useState(["5", "6", "7", "1", "2", "3", "4"]);
+  // const [items, setItems] = useState([
+  //   "6",
+  //   "7",
+  //   "8",
+  //   "9",
+  //   "1",
+  //   "2",
+  //   "3",
+  //   "4",
+  //   "5",
+  // ]);
+
+  const [carouselBWidth, setCarouselBWidth] = useState(1170);
+  const [carouselItemWidth, setCarouselItemWidth] = useState(
+    carouselBWidth / 3
+  );
+  const [carouselBSliderWidth, setCarouselBSliderWidth] = useState(
+    carouselItemWidth * items.length
+  );
+  const [carouselBSliderLeft, setCarouselBSliderLeft] = useState(
+    carouselItemWidth * ((items.length - 3) / 2)
+  );
+  const carouselBContainerStyle = {
+    width: `${carouselBWidth}px`,
+  };
+  const carouselBSliderStyle = {
+    width: `${carouselBSliderWidth}px`,
+    left: `-${carouselBSliderLeft}px`,
+    "@media (max-width: 1800px)": {},
+  };
+
+  const handlePrev = () => {
+    setDirection(1);
+    $slider.current.style.transform = `translate(${carouselItemWidth}px)`;
+  };
+
+  const handleNext = () => {
+    setDirection(-1);
+    $slider.current.style.transform = `translate(-${carouselItemWidth}px)`;
+  };
+
+  const shiftItem = () => {
+    const _items = [...items]; // 深拷貝(淺拷貝會影響到原陣列)
+    if (direction === -1) {
+      console.log(-1);
+      const shiftItem = _items.shift();
+      _items.push(shiftItem);
+      setItems(_items);
+    }
+    if (direction === 1) {
+      console.log(1);
+      const popItem = _items.pop();
+      _items.unshift(popItem);
+      setItems(_items);
+    }
+    $slider.current.style.transition = "none";
+    $slider.current.style.transform = `translate(0px)`;
+    setTimeout(() => {
+      $slider.current.style.transition = "0.3s";
+    }, 4);
+  };
+
+  useEffect(() => {
+    console.log(carouselItemWidth);
+  }, [carouselItemWidth]);
+
+  useEffect(() => {
+    console.log(items);
+  }, [items]);
+
+  return (
+    <section
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {/* carousel */}
+      <div id="carouselB-container" style={carouselBContainerStyle}>
+        <div id="carouselB-wrap" ref={$carousel}>
+          <ul
+            id="carouselB-slider"
+            // className={sliderClassName}
+            onTransitionEnd={shiftItem}
+            style={carouselBSliderStyle}
+            ref={$slider}
+          >
+            {items.map((item, index) => (
+              <>
+                <li key={index}>{item}</li>
+              </>
+            ))}
+          </ul>
+          <div
+            id="carouselB-prev"
+            className="carouselB-btn"
+            onClick={handlePrev}
+            style={{ top: "calc(50% - 50px)" }}
+          >
+            <ArrowLeft
+              style={{
+                width: "100px",
+                height: "100px",
+              }}
+            ></ArrowLeft>
+          </div>
+          <div
+            id="carouselB-next"
+            className="carouselB-btn"
+            onClick={handleNext}
+            style={{ top: "calc(50% - 50px)" }}
+          >
+            <ArrowRight
+              style={{
+                width: "100px",
+                height: "100px",
+              }}
+            ></ArrowRight>
+          </div>
+        </div>
+      </div>
+      {/* carousel */}
+    </section>
+  );
+}
+
+export default Radium(CarouselModuleB);

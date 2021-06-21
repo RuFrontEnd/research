d6763ab4b0a4d52c96e609b357085b1306c64087
RuFrontEnd <k718198@gmail.com>
2021-06-21 21:08:25 +0800

    carouselB merge

--- research-web/src/components/carousel/Carousel.js
+++ research-web/src/components/carousel/Carousel.js
@@ -1,44 +1,8 @@
-// npm i react-reveal --save (如有版本問題使用 npm i react-reveal --save --force)
-// yarn add react-reveal
-// https://www.react-reveal.com/
-
-import "components/carousel/Carousel.css";
-import logo from "assets/logo.png";
-import Zoom from "react-reveal/Zoom";
-import Fade from "react-reveal/Fade";
-import Slide from "react-reveal/Slide";
+import CarouselModule from "components/carousel/CarouselModule";
 
 function Carousel() {
-  return (
-    <>
-      <div>
-        <header className="App-header">
-          <h1 className="App-title">01</h1>
-          <Zoom>
-            <img src={logo} className="App-logo" alt="logo" />
-          </Zoom>
-          <h1 className="App-title">02</h1>
-          <Slide bottom>
-            <div>
-              <img src={logo} className="App-logo" alt="logo" />
-            </div>
-          </Slide>
-          <h1 className="App-title">03</h1>
-          <Fade left>
-            <img src={logo} className="App-logo" alt="logo" />
-          </Fade>
-          <h1 className="App-title">04</h1>
-          <Fade top big cascade>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              <img src={logo} className="App-logo" alt="logo" />
-              <img src={logo} className="App-logo" alt="logo" />
-              <img src={logo} className="App-logo" alt="logo" />
-            </div>
-          </Fade>
-        </header>
-      </div>
-    </>
-  );
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return <CarouselModule activation={0} cards={cards} />;
 }
 
 export default Carousel;

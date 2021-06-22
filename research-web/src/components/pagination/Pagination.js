93f24ec9203a630121eab88322c9975184cb273a
RuFrontEnd <k718198@gmail.com>
2021-06-22 19:35:15 +0800

    整理資料來源

--- research-web/src/components/pagination/Pagination.js
+++ research-web/src/components/pagination/Pagination.js
@@ -1,11 +1,8 @@
-// upload file tutorial https://pjchender.blogspot.com/2019/01/js-javascript-input-file-upload-file.html
-// Cropper.js https://github.com/fengyuanchen/cropperjs
-// Cropper.js tutorial https://www.youtube.com/watch?v=hM9uKmy-BQQ&ab_channel=ThePolyglotDeveloper
-import { useCallback, useEffect, useState } from "react";
-import "components/pagination/Pagination.css";
 // https://jsonplaceholder.typicode.com/ 假API供應
 // https://jsonplaceholder.typicode.com/todos 假todo資料
 // https://www.youtube.com/watch?v=6DtBw3PaeHs tutorial
+import { useCallback, useEffect, useState } from "react";
+import "components/pagination/Pagination.css";
 
 const renderData = (todos) => {
   return (

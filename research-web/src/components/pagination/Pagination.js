7972108778710a94dc5678ee897f4594560d70d3
RuFrontEnd <k718198@gmail.com>
2021-06-22 19:18:40 +0800

    顯示至多5頁換頁按鈕

--- research-web/src/components/pagination/Pagination.js
+++ research-web/src/components/pagination/Pagination.js
@@ -21,6 +21,9 @@ function Pagination() {
   const [todos, setTodos] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [pageNumberLimit, setPageNumberLimit] = useState(5);
+  const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5);
+  const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
 
   const pages = []; // 總頁數
   for (let i = 1; i < Math.ceil(todos.length / itemsPerPage); i++) {
@@ -28,20 +31,52 @@ function Pagination() {
   } // 總共幾頁
 
   const handleCurrentPage = useCallback((e) => {
-    setCurrentPage(e.target.id);
+    setCurrentPage(Number(e.target.id));
   }, []);
 
+  const handlePrevButton = useCallback(
+    (e) => {
+      setCurrentPage((currentPage) => currentPage - 1);
+      if (currentPage - 1 === minPageNumberLimit) {
+        setMaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+        setMinPageNumberLimit(minPageNumberLimit - pageNumberLimit);
+      }
+    },
+    [currentPage]
+  );
+
+  useEffect(() => {
+    console.log("currentPage", currentPage);
+    console.log("maxPageNumberLimit", maxPageNumberLimit);
+    console.log("minPageNumberLimit", minPageNumberLimit);
+  }, [currentPage, maxPageNumberLimit, minPageNumberLimit]);
+
+  const handleNextButton = useCallback(
+    (e) => {
+      setCurrentPage((currentPage) => currentPage + 1);
+      if (currentPage + 1 > maxPageNumberLimit) {
+        setMaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+        setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit);
+      }
+    },
+    [currentPage]
+  ); // dependecy表示該state改變時才會影響到useCallback內的state值(所有)
+
   const renderPageNumbers = pages.map((number) => {
-    return (
-      <li
-        key={number}
-        id={number}
-        className={"page"}
-        onClick={handleCurrentPage}
-      >
-        {number}
-      </li>
-    );
+    if (number > minPageNumberLimit && number < maxPageNumberLimit + 1) {
+      return (
+        <li
+          key={number}
+          id={number}
+          onClick={handleCurrentPage}
+          className={`page ${currentPage === number ? "active" : null}`}
+        >
+          {number}
+        </li>
+      );
+    } else {
+      return null;
+    }
   }); // 頁數元件
 
   const indexOfLastItem = currentPage * itemsPerPage; // 每頁最後一筆資料索引
@@ -68,7 +103,21 @@ function Pagination() {
     <>
       <h1>Todo List</h1>
       {renderData(currentItems)}
-      <ul id="pages">{renderPageNumbers}</ul>
+      <ul id="pages">
+        <li className="page-button" onClick={handlePrevButton}>
+          <button disabled={currentPage === pages[0] ? true : false}>
+            PREV
+          </button>
+        </li>
+        {renderPageNumbers}
+        <li className="page-button" onClick={handleNextButton}>
+          <button
+            disabled={currentPage === pages[pages.length - 1] ? true : false}
+          >
+            NEXT
+          </button>
+        </li>
+      </ul>
     </>
   );
 }

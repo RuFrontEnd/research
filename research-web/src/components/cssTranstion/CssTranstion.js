c74a4edfcee7ccaf57ed9cb41ec2ec3caaa96551
RuFrontEnd <k718198@gmail.com>
2021-07-04 20:53:01 +0800

    實作Dom生成之間的過渡

--- research-web/src/components/cssTranstion/CssTranstion.js
+++ research-web/src/components/cssTranstion/CssTranstion.js
@@ -1,8 +1,43 @@
-// import React from 'react'
+// tutorial http://reactcommunity.org/react-transition-group/css-transition
+// npm i react-transition-group
+// 用於當dom結構消失與產生時的過渡
+
+import { useState } from "react";
+import { CSSTransition } from "react-transition-group";
 import "components/cssTranstion/CssTranstion.css";
 
 function CssTranstion() {
-  return <section></section>;
+  const [showHello, setShowHello] = useState(true);
+
+  return (
+    <section>
+      <button
+        onClick={() => {
+          setShowHello(!showHello);
+        }}
+      >
+        switchMessage
+      </button>
+      <div style={{ position: "relative" }}>
+        <CSSTransition
+          in={showHello}
+          timeout={10 * 1000} // 持續多久 => 通常跟css transtion屬性同時間
+          classNames="alert"
+          unmountOnExit
+        >
+          <div>hello</div>
+        </CSSTransition>
+        <CSSTransition
+          in={!showHello}
+          timeout={10 * 1000} // 持續多久 => 通常跟css transtion屬性同時間
+          classNames="alert"
+          unmountOnExit
+        >
+          <div>world</div>
+        </CSSTransition>
+      </div>
+    </section>
+  );
 }
 
 export default CssTranstion;

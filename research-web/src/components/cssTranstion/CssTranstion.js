3c5f5efb42d445382ed9ce01af89156defeda6d0
RuFrontEnd <k718198@gmail.com>
2021-07-04 20:26:08 +0800

    Revert "create cssTranstion and Template component"
    This reverts commit f03f9cb66bf09fcbb1aec9392dff00fb47411729.

---
+++ research-web/src/components/cssTranstion/CssTranstion.js
@@ -0,0 +1,8 @@
+// import React from 'react'
+import "components/CssTranstion.js/CssTranstion.css";
+
+function CssTranstion() {
+  return <section></section>;
+}
+
+export default CssTranstion;
---
+++ research-web/src/components/template/Template.js
@@ -0,0 +1,8 @@
+// import React from 'react'
+import "components/template/Template.css";
+
+function Template() {
+  return <section></section>;
+}
+
+export default Template;

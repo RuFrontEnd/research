f03f9cb66bf09fcbb1aec9392dff00fb47411729
RuFrontEnd <k718198@gmail.com>
2021-07-04 20:20:10 +0800

    create cssTranstion and Template component

--- research-web/src/components/cssTranstion/CssTranstion.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react'
-import "components/CssTranstion.js/CssTranstion.css";
-
-function CssTranstion() {
-  return <section></section>;
-}
-
-export default CssTranstion;
--- research-web/src/components/template/Template.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react'
-import "components/template/Template.css";
-
-function Template() {
-  return <section></section>;
-}
-
-export default Template;

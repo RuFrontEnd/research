44c21e52137c2e10fb2787fa77427f92452402d0
RuFrontEnd <k718198@gmail.com>
2021-07-04 20:21:27 +0800

    Revert "create CssTranstion component"
    This reverts commit 41a3a0dfb014291f1fc96fab64c6a9d508ad784a.

---
+++ research-web/src/App.js
@@ -0,0 +1,46 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom"; // npm install react-router-dom
+import UploadFile from "components/uploadFile/UploadFile";
+import Reveal from "components/reveal/Reveal";
+import Carousel from "components/carousel/Carousel";
+import CarouselB from "components/carouselB/CarouselB";
+import Pagination from "components/pagination/Pagination";
+import Internationalization from "components/internationalization/Internationalization";
+import Nav from "components/Nav";
+// import CSSTransition from "react-transition-group/CSSTransition"
+import { CSSTransition } from "react-transition-group";
+import "App.css";
+
+const routes = [
+  { path: "/", component: <Nav style={{ padding: "30px" }} /> },
+  { path: "/internation", component: <Internationalization /> },
+  { path: "/pagination", component: <Pagination /> },
+  { path: "/carouselB", component: <CarouselB /> },
+  { path: "/carousel", component: <Carousel /> },
+  { path: "/reveal", component: <Reveal /> },
+  { path: "/uploadFile", component: <UploadFile /> },
+];
+
+function App() {
+  return (
+    <div style={{ position: "relative" }}>
+      <BrowserRouter>
+        {routes.map((route) => (
+          <Route exact path={route.path}>
+            {({ match }) => (
+              <CSSTransition
+                in={match != null}
+                timeout={300}
+                classNames="transition"
+                unmountOnExit
+              >
+                {route.component}
+              </CSSTransition>
+            )}
+          </Route>
+        ))}
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;

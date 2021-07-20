4ede8899fafbb27f4e532de0606fad7de6a9c4ff
RuFrontEnd <k718198@gmail.com>
2021-07-20 22:08:35 +0800

    const state

--- research-web/src/components/setStateInUseEffect/SetStateInUseEffect.js
+++ research-web/src/components/setStateInUseEffect/SetStateInUseEffect.js
@@ -1,7 +1,10 @@
-// import React from 'react'
+import React, { useState, useEffect } from "react";
 import "components/template/Template.css";
 
 function SetStateInUseEffect() {
+  const [state, useState] = useState('')
+
+
   return <section></section>;
 }
 

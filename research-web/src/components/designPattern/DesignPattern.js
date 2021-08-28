219481e23aa8e4708fef38ef4841facd1f8b21eb
RuFrontEnd <k718198@gmail.com>
2021-08-28 13:10:06 +0800

    物件新增方式

--- research-web/src/components/designPattern/DesignPattern.js
+++ research-web/src/components/designPattern/DesignPattern.js
@@ -5,9 +5,49 @@ function DesignPattern() {
   const object1 = {};
   const object2 = Object.create(Object.prototype);
   const object3 = new Object();
-  console.log(object1, object2, object3);
+  // console.log(object1, object2, object3);
 
-  return <section></section>;
+  const myObject = {};
+  myObject.key1 = "Hello World";
+  myObject["key2"] = "Hi World";
+  Object.defineProperty(myObject, "key3", {
+    value: "Hey, World",
+    writable: true,
+    enumberalbe: true,
+    configurable: true,
+  });
+  Object.defineProperties(myObject, {
+    key3: {
+      value: "Ha World",
+      writable: true,
+    },
+    key4: {
+      value: "Hoooo World",
+      writable: false,
+    },
+  });
+  // console.log(myObject);
+
+  // constructor pattern 建構子模式
+  function Student(name, year, grade) {
+    this.name = name;
+    this.year = year;
+    this.grade = grade;
+  }
+  Student.prototype.toString = function () {
+    return `Name:${this.name} Year:${this.year} Grade:${this.grade}`;
+  }; // 利用prototype的方式可避免浪費記憶體空間
+  const student1 = new Student("小明", 19, "大一");
+  // console.log(student1);
+
+  // Module Pattern 模組模式
+
+
+  console.log("publicAPI", publicAPI);
+
+  return (
+    <section style={{ fontSize: "20px" }}>check the console first plz.</section>
+  );
 }
 
 export default DesignPattern;

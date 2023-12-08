524aa2c81f2bb488d35104efe57954f97451d2b4
Luke <luke_xie@wistron.com>
2023-12-08 08:57:36 +0800

    新增 curve shape

--- process-whiteboard/src/shapes/curve/index.tsx
+++ process-whiteboard/src/shapes/curve/index.tsx
@@ -1,8 +1,5 @@
 "use client";
-import { useRef, useEffect, useCallback } from "react";
 import Arrow from "@/shapes/arrow";
-
-let ctx: CanvasRenderingContext2D | null | undefined = null;
 
 type Vec = { x: number; y: number };
 type Line = {
@@ -15,21 +12,8 @@ enum PressingP {
   p2 = "p2",
   cp2 = "cp2",
 }
-enum ShapeType {
-  curve = "curve",
-}
 
-const initPressingShape = {
-  type: null,
-  index: -1,
-};
-
-let pressingShape: {
-  type: ShapeType | null;
-  index: number;
-} = initPressingShape;
-
-class Curve {
+export default class Curve {
   private initPressing = {
     activate: false,
     p: null,
@@ -273,104 +257,4 @@ class Curve {
         90 * (Math.PI / 180)
     );
   }
-}
-
-let shapes: any[] = [];
-
-export default function BezierCurve() {
-  let { current: $canvas } = useRef<HTMLCanvasElement | null>(null);
-
-  const onMouseDown = useCallback((e: React.MouseEvent<HTMLCanvasElement>) => {
-    let $canvas = document.querySelector("canvas");
-    if (!ctx || !$canvas) return;
-    let cpline = {
-        w: 1,
-        c: "#c00",
-      },
-      curve = {
-        w: 6,
-        c: "#333",
-      },
-      pressingP = {
-        x: e.nativeEvent.x,
-        y: e.nativeEvent.y,
-      };
-
-    shapes.forEach((shape, shapeI) => {
-      if (shape.checkBoundry($canvas, pressingP)) {
-        pressingShape = {
-          type: ShapeType.curve,
-          index: shapeI,
-        };
-      }
-    });
-
-    if (pressingShape.index > -1) {
-      shapes[pressingShape.index].onMouseDown($canvas);
-      return;
-    }
-
-    // if(type === 'curve'){ // TODO: 待之後有不同工具時做判斷
-    let newCurve = new Curve(cpline, curve, 300);
-    shapes.push(newCurve);
-    newCurve.init(pressingP);
-    // }
-  }, []);
-
-  const onMouseMove = useCallback((e: React.MouseEvent<HTMLCanvasElement>) => {
-    shapes.forEach((shape) => {
-      if (shape instanceof Curve) {
-        shape.onMouseMove({
-          x: e.nativeEvent.x,
-          y: e.nativeEvent.y,
-        });
-      }
-    });
-  }, []);
-
-  const onMouseUp = useCallback((e: React.MouseEvent<HTMLCanvasElement>) => {
-    let $canvas = document.querySelector("canvas");
-    if (!$canvas) return;
-
-    shapes.forEach((shape) => {
-      if (!$canvas) return;
-      if (shape instanceof Curve) {
-        shape.onMouseUp($canvas);
-      }
-    });
-    pressingShape = initPressingShape;
-  }, []);
-
-  const draw = () => {
-    if (!ctx) return;
-    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-
-    shapes.forEach((shape) => {
-      if (!ctx) return;
-      if (shape instanceof Curve) {
-        shape.draw(ctx);
-      }
-    });
-    window.requestAnimationFrame(draw);
-  };
-
-  useEffect(() => {
-    if ($canvas) {
-      $canvas.width = window.innerWidth;
-      $canvas.height = window.innerHeight;
-      window.requestAnimationFrame(draw);
-    }
-  }, []);
-
-  return (
-    <canvas
-      ref={(el) => {
-        $canvas = el;
-        ctx = $canvas?.getContext("2d");
-      }}
-      onMouseDown={onMouseDown}
-      onMouseMove={onMouseMove}
-      onMouseUp={onMouseUp}
-    />
-  );
 }
